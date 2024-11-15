eb314668e603dc7a59b83d9055164ddf6f15a845
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-11-15 19:41:24 +0000

    y2020d01

--- src/run.rs
+++ src/run.rs
@@ -26,21 +26,23 @@ pub fn run(params: &DayParams) -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
+type SolutionFn = fn(String) -> String;
+
 fn run_solution(year: &i32, day: &u32) -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string(format!("input/y{year}d{:02}.txt", day))?;
 
     println!("year {year} day {:02}", day);
     println!("=====================");
     // find a reasonable way to get the right sol functions (macros?)
-    let sol1 = match (year, day) {
-        (2015, 1) => y2015d01::part1(input.clone()),
-        _ => panic!("!"),
+    let (part1, part2): (SolutionFn, SolutionFn) = match (year, day) {
+        (2015, 1) => (y2015d01::part1, y2015d01::part2),
+        (2020, 1) => (y2020d01::part1, y2020d01::part2),
+        _ => panic!("Solution not found"),
     };
+
+    let sol1 = part1(input.clone());
     println!("Part 1: {sol1}");
-    let sol2 = match (year, day) {
-        (2015, 1) => y2015d01::part2(input.clone()),
-        _ => panic!("!"),
-    };
+    let sol2 = part2(input.clone());
     println!("Part 2: {sol2}");
     Ok(())
 }
