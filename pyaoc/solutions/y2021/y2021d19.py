19b7f89e69f2732ee04176534e3902cc4617b38d
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-11-22 17:18:35 +0000

    Draft python 2021/19

---
+++ pyaoc/solutions/y2021/y2021d19.py
@@ -0,0 +1,34 @@
+from typing import TypeAlias
+from collections import Counter
+
+
+def part1(input_str: str) -> str:
+    scanners = parse_input(input_str)
+    print(Counter(len(s) for s in scanners))
+    return str(len(scanners))
+
+
+def part2(input_str: str) -> str:
+    return "ciao"
+
+
+Scanner: TypeAlias = list[tuple[int, int, int]]
+
+
+def parse_input(input_str: str) -> list[Scanner]:
+    scanners = []
+    scanner: Scanner = []
+    for line in input_str.splitlines():
+        if line.startswith("--- scanner"):
+            if scanner:
+                scanners.append(scanner)
+            scanner = []
+        elif line == "":
+            continue
+        else:
+            coords = tuple(map(int, line.split(",")))
+            assert len(coords) == 3
+            scanner.append(coords)
+    scanners.append(scanner)
+
+    return scanners
