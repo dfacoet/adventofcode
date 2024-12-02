9f619fa4cb694f4eeae420b03530c783182fc297
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-12-02 20:50:53 +0000

    Haskell style improvements 2024-02

--- haskell/src/Year2024/Day02.hs
+++ haskell/src/Year2024/Day02.hs
@@ -1,16 +1,16 @@
 module Year2024.Day02 (part1, part2) where
 
 part1 :: String -> String
-part1 input = show $ length $ filter isValid (parseInput input)
+part1 input = show . length $ filter isValid (parseInput input)
 
 part2 :: String -> String
-part2 input = show $ length $ filter (any isValid . dropEach) (parseInput input)
+part2 input = show . length $ filter (any isValid . dropEach) (parseInput input)
 
 parseInput :: String -> [[Integer]]
 parseInput input = map (map read . words) (lines input)
 
 diff :: (Num a) => [a] -> [a]
-diff xs = zipWith (-) xs (tail xs)
+diff = zipWith (-) <*> tail
 
 isValid :: (Eq a, Num a) => [a] -> Bool
 isValid levels =
