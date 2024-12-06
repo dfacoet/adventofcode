c5b48b939c2c09cba26ddf30712886615ace32f4
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-12-06 08:38:30 +0000

    2024-06-p2 Rust

--- rust/y2024/d06/y2024d06.rs
+++ rust/y2024/d06/y2024d06.rs
@@ -2,11 +2,44 @@ use core::panic;
 use std::collections::HashSet;
 
 pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
-    // Solve part 1
+    let (guard_pos, obstacles, grid_bounds) = parse_input(input)?;
+
+    if let Some(n_visited) = run_guard(guard_pos, &obstacles, grid_bounds) {
+        Ok(format!("{}", n_visited))
+    } else {
+        Err("Guard is stuck in a loop".into())
+    }
+}
+
+pub fn part2(input: String) -> Result<String, Box<dyn std::error::Error>> {
+    let (guard_pos, obstacles, grid_bounds) = parse_input(input)?;
+
+    let mut count = 0;
+    for i in 1..grid_bounds.0 {
+        for j in 1..grid_bounds.1 {
+            let mut new_obstacles = obstacles.clone();
+            if (i, j) != guard_pos && new_obstacles.insert((i, j)) {
+                match run_guard(guard_pos, &new_obstacles, grid_bounds) {
+                    Some(_) => { // guard is not stuck in a loop
+                    }
+                    None => count += 1,
+                }
+            }
+        }
+    }
+
+    Ok(format!("{}", count))
+}
+
+type Coord = (usize, usize);
+
+fn parse_input(
+    input: String,
+) -> Result<(Coord, HashSet<Coord>, Coord), Box<dyn std::error::Error>> {
     let mut obstacles = HashSet::new();
     let mut guard_pos = (0, 0);
-    let mut guard_dir = 0;
     let mut grid_bounds = (0, 0);
+
     for (i, row) in input.split('\n').enumerate() {
         for (j, c) in row.chars().enumerate() {
             match c {
@@ -23,22 +56,8 @@ pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
         }
     }
 
-    let mut visited = HashSet::new();
-    visited.insert(guard_pos);
-    while let Some((new_pos, new_dir)) = get_next(guard_pos, guard_dir, &obstacles, grid_bounds) {
-        (guard_pos, guard_dir) = (new_pos, new_dir);
-        visited.insert(guard_pos);
-    }
-
-    Ok(format!("{}", visited.len()))
+    Ok((guard_pos, obstacles, grid_bounds))
 }
-
-pub fn part2(_input: String) -> Result<String, Box<dyn std::error::Error>> {
-    // Solve part 2
-    Err("Solution not implemented".into())
-}
-
-type Coord = (usize, usize);
 
 fn get_next(pos: Coord, dir: u8, obstacles: &HashSet<Coord>, bounds: Coord) -> Option<(Coord, u8)> {
     let next_cell = match dir {
@@ -55,4 +74,24 @@ fn get_next(pos: Coord, dir: u8, obstacles: &HashSet<Coord>, bounds: Coord) -> O
     } else {
         Some((next_cell, dir))
     }
+}
+
+fn run_guard(initial_pos: Coord, obstacles: &HashSet<Coord>, grid_bounds: Coord) -> Option<usize> {
+    let mut guard_dir = 0;
+    let mut guard_pos = initial_pos;
+
+    let mut visited = HashSet::new();
+    visited.insert((guard_pos, guard_dir));
+    while let Some((new_pos, new_dir)) = get_next(guard_pos, guard_dir, obstacles, grid_bounds) {
+        (guard_pos, guard_dir) = (new_pos, new_dir);
+        if !visited.insert((guard_pos, guard_dir)) {
+            return None;
+        }
+    }
+    let n_visited_cells = visited
+        .iter()
+        .map(|(p, _)| p)
+        .collect::<HashSet<&Coord>>()
+        .len();
+    Some(n_visited_cells)
 }
