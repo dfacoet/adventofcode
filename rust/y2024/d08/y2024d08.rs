96f61ce7bb6cd69e0beb68dfa21dbc23470c158d
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-12-08 12:51:57 +0000

    2024-08-p2 Rust

--- rust/y2024/d08/y2024d08.rs
+++ rust/y2024/d08/y2024d08.rs
@@ -5,20 +5,24 @@ type Coord = (i64, i64);
 pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
     let (antennas, grid_shape) = parse_input(input);
 
-    let antinodes: HashSet<(i64, i64)> = antennas
+    let antinodes: HashSet<Coord> = antennas
         .values()
         .flat_map(get_antinodes)
-        .filter(|(x, y)| *x >= 0 && *x < grid_shape.0 && *y >= 0 && *y < grid_shape.1)
+        .filter(|p| in_grid(*p, grid_shape))
         .collect();
 
     Ok(antinodes.len().to_string())
 }
 
-// 302 is too high
+pub fn part2(input: String) -> Result<String, Box<dyn std::error::Error>> {
+    let (antennas, grid_shape) = parse_input(input);
 
-pub fn part2(_input: String) -> Result<String, Box<dyn std::error::Error>> {
-    // Solve part 2
-    Err("Solution not implemented".into())
+    let antinodes: HashSet<Coord> = antennas
+        .values()
+        .flat_map(|ps| get_antinodes2(ps, grid_shape))
+        .collect();
+
+    Ok(antinodes.len().to_string())
 }
 
 fn parse_input(input: String) -> (HashMap<char, Vec<Coord>>, Coord) {
@@ -41,15 +45,39 @@ fn parse_input(input: String) -> (HashMap<char, Vec<Coord>>, Coord) {
     (antennas, grid_shape)
 }
 
-fn get_antinodes2((x1, y1): Coord, (x2, y2): Coord) -> Vec<Coord> {
-    // TODO: check for internal antinodes
-    vec![(2 * x1 - x2, 2 * y1 - y2), (2 * x2 - x1, 2 * y2 - y1)]
+// TODO: make generic and move to shared library
+// Consider adding trait for iterators, or using external crate
+fn pairs(xs: &Vec<Coord>) -> impl Iterator<Item = (&Coord, &Coord)> {
+    xs.iter()
+        .enumerate()
+        .flat_map(|(i, a)| xs.iter().skip(i + 1).map(move |b| (a, b)))
 }
 
 fn get_antinodes(ps: &Vec<Coord>) -> HashSet<Coord> {
-    ps.iter()
-        .enumerate()
-        .flat_map(|(i, a)| ps.iter().skip(i + 1).map(move |b| (a, b))) // combinations(ps, ps)
-        .flat_map(|(p1, p2)| get_antinodes2(*p1, *p2))
+    pairs(ps)
+        .flat_map(|((x1, y1), (x2, y2))| {
+            // TODO: check for internal antinodes
+            vec![(2 * x1 - x2, 2 * y1 - y2), (2 * x2 - x1, 2 * y2 - y1)]
+        })
+        .collect()
+}
+
+fn in_grid((x, y): Coord, (xmax, ymax): Coord) -> bool {
+    x >= 0 && x < xmax && y >= 0 && y < ymax
+}
+
+fn get_antinodes2(ps: &Vec<Coord>, grid_shape: Coord) -> HashSet<Coord> {
+    pairs(ps)
+        .flat_map(|((x1, y1), (x2, y2))| {
+            let (dx, dy) = (x2 - x1, y2 - y1);
+            (0..)
+                .map(move |k| (x1 - k * dx, y1 - k * dy))
+                .take_while(|p| in_grid(*p, grid_shape))
+                .chain(
+                    (0..)
+                        .map(move |k| (x1 + k * dx, y1 + k * dy))
+                        .take_while(|p| in_grid(*p, grid_shape)),
+                )
+        })
         .collect()
 }
