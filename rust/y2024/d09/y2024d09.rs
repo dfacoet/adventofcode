23286303177f298b8f69aed7347abd33534f5a38
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-12-10 19:41:11 +0000

    2024-09-p2 Rust

--- rust/y2024/d09/y2024d09.rs
+++ rust/y2024/d09/y2024d09.rs
@@ -1,32 +1,22 @@
 use std::collections::VecDeque;
 
 pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
-    let (mut files, empties) = parse_input(input);
-    let ps: Vec<_> = std::iter::once(0)
-        .chain(
-            files
-                .iter()
-                .zip(empties.iter())
-                .map(|(x, y)| x + y)
-                .scan(0, |acc, x| {
-                    *acc += x; // cumsum
-                    Some(*acc)
-                }),
-        )
-        .collect();
+    let (files, empties) = parse_input(input);
+    let ps = cumsum(component_sum(&files, &empties));
+    let mut files = VecDeque::from(files);
     let checksum: usize = empties
         .iter()
         .enumerate()
         .map_while(|(i, e)| {
-            if let Some(file_n_blocks) = files.pop_front() {
+            if let Some(file_size) = files.pop_front() {
                 let file_start = *ps.get(i).unwrap();
                 let eval: usize = (0..*e)
                     .map_while(|j| {
                         let file_id = i + get_from_end(&mut files)?;
-                        Some((file_start + file_n_blocks + j) * file_id)
+                        Some((file_start + file_size + j) * file_id)
                     })
                     .sum();
-                Some(file_checksum(i, file_n_blocks, file_start) + eval)
+                Some(file_checksum(i, file_size, file_start) + eval)
             } else {
                 None
             }
@@ -35,12 +25,38 @@ pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
     Ok(checksum.to_string())
 }
 
-pub fn part2(_input: String) -> Result<String, Box<dyn std::error::Error>> {
-    // Solve part 2
-    Err("Solution not implemented".into())
+pub fn part2(input: String) -> Result<String, Box<dyn std::error::Error>> {
+    let (files, empties) = parse_input(input);
+    let file_pos = cumsum(component_sum(&files, &empties));
+    let mut empties: Vec<_> = cumsum(component_sum(&files[1..], &empties))
+        .into_iter()
+        .map(|p| p + files[0])
+        .zip(empties)
+        .collect(); // [(pos, size)]
+
+    let files = file_pos.into_iter().zip(files); // [(pos, size)]
+
+    let mut total = 0;
+    for (f_id, (f_pos, f_size)) in files.enumerate().rev() {
+        let pos = if let Some((ref mut e_pos, ref mut e_size)) = empties
+            .iter_mut()
+            .take_while(|(p, _)| *p < f_pos)
+            .find(|(_, s)| *s >= f_size)
+        {
+            let new_pos = *e_pos;
+            *e_pos += f_size;
+            *e_size -= f_size;
+            new_pos
+        } else {
+            f_pos
+        };
+        total += file_checksum(f_id, f_size, pos);
+    }
+
+    Ok(total.to_string())
 }
 
-fn parse_input(input: String) -> (VecDeque<usize>, Vec<usize>) {
+fn parse_input(input: String) -> (Vec<usize>, Vec<usize>) {
     let input_int: Vec<_> = input
         .trim()
         .chars()
@@ -50,6 +66,21 @@ fn parse_input(input: String) -> (VecDeque<usize>, Vec<usize>) {
     let empties = input_int.iter().skip(1).step_by(2).copied().collect();
 
     (files, empties)
+}
+
+// TODO: make generic and move to shared library
+// Consider adding trait for iterators, or using external crate
+fn cumsum(xs: Vec<usize>) -> Vec<usize> {
+    std::iter::once(0)
+        .chain(xs.iter().scan(0, |acc, x| {
+            *acc += x;
+            Some(*acc)
+        }))
+        .collect()
+}
+
+fn component_sum(xs: &[usize], ys: &[usize]) -> Vec<usize> {
+    xs.iter().zip(ys.iter()).map(|(x, y)| x + y).collect()
 }
 
 fn get_from_end(files: &mut VecDeque<usize>) -> Option<usize> {
@@ -64,6 +95,6 @@ fn get_from_end(files: &mut VecDeque<usize>) -> Option<usize> {
     }
 }
 
-fn file_checksum(id: usize, n_blocks: usize, pos: usize) -> usize {
-    id * n_blocks * (2 * pos + n_blocks - 1) / 2
+fn file_checksum(id: usize, size: usize, pos: usize) -> usize {
+    id * size * (2 * pos + size - 1) / 2
 }
