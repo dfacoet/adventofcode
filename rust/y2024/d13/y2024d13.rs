93044cd414f0f833766e071b97df23428ea7c506
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-12-13 17:18:34 +0100

    2024-13 Rust

--- rust/y2024/d13/y2024d13.rs
+++ rust/y2024/d13/y2024d13.rs
@@ -1,23 +1,27 @@
 use std::str::FromStr;
 
 pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
-    let problems = parse_input(input)?;
-    Ok(problems
-        .iter()
-        .filter_map(naive_solver)
+    Ok(input
+        .split("\n\n")
+        .map(Problem::from_str)
+        .filter_map(|res| res.ok().and_then(|p| solver(&p, 0)))
         .sum::<u64>()
         .to_string())
 }
 
-pub fn part2(_input: String) -> Result<String, Box<dyn std::error::Error>> {
-    // Solve part 2
-    Err("Solution not implemented".into())
+pub fn part2(input: String) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(input
+        .split("\n\n")
+        .map(Problem::from_str)
+        .filter_map(|res| res.ok().and_then(|p| solver(&p, 10000000000000)))
+        .sum::<u64>()
+        .to_string())
 }
 
 #[derive(Debug)]
 struct Problem {
-    d_a: (u64, u64),
-    d_b: (u64, u64),
+    da: (u64, u64),
+    db: (u64, u64),
     target: (u64, u64),
 }
 
@@ -28,42 +32,41 @@ impl std::str::FromStr for Problem {
         let mut lines = s.lines();
 
         let parse_coords = |line: &str| -> Result<(u64, u64), Box<dyn std::error::Error>> {
-            let parts: Vec<&str> = line.split(&[',', ' ', ':', '+', '='][..]).collect();
+            let parts: Vec<&str> = line.split(&[' ', ',', '+', '=']).collect();
             let x = parts.iter().position(|&s| s == "X").ok_or("Missing X")?;
             let y = parts.iter().position(|&s| s == "Y").ok_or("Missing Y")?;
             Ok((parts[x + 1].parse()?, parts[y + 1].parse()?))
         };
 
         Ok(Problem {
-            d_a: parse_coords(lines.next().ok_or("Missing Button A")?)?,
-            d_b: parse_coords(lines.next().ok_or("Missing Button B")?)?,
+            da: parse_coords(lines.next().ok_or("Missing Button A")?)?,
+            db: parse_coords(lines.next().ok_or("Missing Button B")?)?,
             target: parse_coords(lines.next().ok_or("Missing Prize")?)?,
         })
     }
 }
 
-impl Problem {
-    fn on_target(&self, (p_a, p_b): (u64, u64)) -> bool {
-        (p_a * self.d_a.0 + p_b * self.d_b.0 == self.target.0)
-            && (p_a * self.d_a.1 + p_b * self.d_b.1 == self.target.1)
+fn solver(p: &Problem, shift: u64) -> Option<u64> {
+    let ((d1, d2), (c1, c2)) = if slope(p.da) < slope(p.db) {
+        ((p.da, p.db), (3, 1))
+    } else {
+        ((p.db, p.da), (1, 3))
+    };
+    let target = (p.target.0 + shift, p.target.1 + shift);
+    if slope(d1) > slope(target) || slope(target) > slope(d2) {
+        return None;
+    }
+    let num1 = d2.1 * target.0 - d2.0 * target.1;
+    let num2 = d1.0 * target.1 - d1.1 * target.0;
+    let den = d2.1 * d1.0 - d2.0 * d1.1;
+
+    if num1 % den == 0 && num2 % den == 0 {
+        Some(c1 * num1 / den + c2 * num2 / den)
+    } else {
+        None
     }
 }
 
-fn parse_input(input: String) -> Result<Vec<Problem>, Box<dyn std::error::Error>> {
-    input.split("\n\n").map(Problem::from_str).collect()
-}
-
-fn naive_solver(p: &Problem) -> Option<u64> {
-    let mut solutions = Vec::new();
-    println!("{:?}", p);
-    // TODO: use better limits
-    for b in 0..101 {
-        for a in 0..101 {
-            if p.on_target((a, b)) {
-                println!("{a},{b} is a solution");
-                solutions.push((a, b));
-            }
-        }
-    }
-    solutions.iter().map(|(a, b)| 3 * a + b).min()
+fn slope((i, j): (u64, u64)) -> f32 {
+    j as f32 / i as f32
 }
