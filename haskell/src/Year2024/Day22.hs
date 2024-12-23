08028cc28132bb07fd26b1c0dc38072634a6ca33
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-12-23 01:15:29 +0100

    2024-22-p2 Haskell

--- haskell/src/Year2024/Day22.hs
+++ haskell/src/Year2024/Day22.hs
@@ -1,30 +1,38 @@
 module Year2024.Day22 (part1, part2) where
 
-import Data.Bits (xor)
+import           Data.Bits (shiftL, shiftR, xor, (.&.))
+import           Data.List (tails, transpose)
+import qualified Data.Map  as Map
 
 part1 :: String -> String
-part1 input = show $ sum $ map (evolveN 2000) $ parseInput input
+part1 = show . sum . map (evolveN 2000 . read) . lines
 
 part2 :: String -> String
-part2 = error "Part2 not implemented"
+part2 input =
+  show . maximum . Map.elems $
+    foldl
+      (Map.unionWith (+))
+      Map.empty
+      (map (countSeqs . read) (lines input))
+  where
+    countSeqs n =
+      let digits = (map (`mod` 10) . take 2001 . iterate evolve) n
+          diffs = zipWith (-) (tail digits) digits
+       in foldl
+            (\acc (k, v) -> Map.insertWith (\_ b -> b) k v acc)
+            Map.empty
+            (zip (windows 4 diffs) (drop 4 digits))
 
-pruneAt :: Integer
-pruneAt = 16777216 -- 2^24
+evolve :: Int -> Int
+evolve = step3 . step2 . step1 -- TODO: simplify ops after composing
+  where
+    pruneConst = 16777216 - 1 -- 2^24 - 1
+    step1 n = ((n `shiftL` 6) `xor` n) .&. pruneConst
+    step2 n = ((n `shiftR` 5) `xor` n) .&. pruneConst
+    step3 n = ((n `shiftL` 11) `xor` n) .&. pruneConst
 
-parseInput :: String -> [Integer]
-parseInput s = map read (lines s)
-
-step1 :: Integer -> Integer
-step1 n = ((n * 64) `xor` n) `mod` pruneAt
-
-step2 :: Integer -> Integer
-step2 n = ((n `div` 32) `xor` n) `mod` pruneAt
-
-step3 :: Integer -> Integer
-step3 n = ((n * 2048) `xor` n) `mod` pruneAt
-
-evolve :: Integer -> Integer
-evolve = step3 . step2 . step1
-
-evolveN :: Int -> Integer -> Integer
+evolveN :: Int -> Int -> Int
 evolveN n x = iterate evolve x !! n
+
+windows :: Int -> [a] -> [[a]]
+windows n = takeWhile (\w -> length w == n) . transpose . take n . tails
