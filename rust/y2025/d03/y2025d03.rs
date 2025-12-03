d157486ed3870ee273402cc820bb89390c51dc04
dfacoet <11651258+dfacoet@users.noreply.github.com>
2025-12-03 05:47:46 +0000

    2025-03-p2

--- rust/y2025/d03/y2025d03.rs
+++ rust/y2025/d03/y2025d03.rs
@@ -1,29 +1,38 @@
 pub fn part1(input: String) -> Result<String, Box<dyn std::error::Error>> {
-    let result: Result<u32, Box<dyn std::error::Error>> = input
+    solve(input, 2)
+}
+
+pub fn part2(input: String) -> Result<String, Box<dyn std::error::Error>> {
+    solve(input, 12)
+}
+
+fn solve(input: String, n: usize) -> Result<String, Box<dyn std::error::Error>> {
+    let result: Result<u64, Box<dyn std::error::Error>> = input
         .trim()
         .lines()
-        .map(max_joltage)
+        .map(|r| max_joltage_n(r, n))
         .collect::<Result<Vec<_>, _>>()
         .map(|vec| vec.iter().sum());
     Ok(result?.to_string())
 }
 
-pub fn part2(_input: String) -> Result<String, Box<dyn std::error::Error>> {
-    // Solve part 2
-    Err("Solution not implemented".into())
-}
+fn max_joltage_n(row: &str, n: usize) -> Result<u64, Box<dyn std::error::Error>> {
+    if n == 1 {
+        return row
+            .chars()
+            .max()
+            .and_then(|d| d.to_digit(10))
+            .map(|d| d as u64)
+            .ok_or("Invalid bank".into());
+    }
+    let (i, first_digit) = row[..row.len() - (n - 1)]
+        .char_indices()
+        .rev()
+        .max_by_key(|(_, c)| *c)
+        .ok_or("Bank is too small")?;
 
-fn max_joltage(row: &str) -> Result<u32, Box<dyn std::error::Error>> {
-    let first_digit = row[..row.len() - 1].chars().max().ok_or("Bank is empty")?;
-    let (i, _) = row
-        .chars()
-        .enumerate()
-        .find(|(_, c)| *c == first_digit)
-        .unwrap();
-    // TODO: do it in one go with something like row.chars().enumerate().max_by_key
-    // but max methods return the last value
-    let second_digit = row[i + 1..].chars().max().ok_or("Bank is empty")?;
-    let result = 10 * first_digit.to_digit(10).ok_or("Invalid bank")?
-        + second_digit.to_digit(10).ok_or("Invalid bank")?;
+    // Recursive is slow for long banks, consider iterating over the row only once.
+    let result = 10u64.pow((n - 1) as u32) * first_digit.to_digit(10).ok_or("Invalid bank")? as u64
+        + max_joltage_n(&row[i + 1..], n - 1)?;
     Ok(result)
 }
